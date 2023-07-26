28a552458a9180d47fe8fe79157354749dd185fb
Ivan Romero <ivanromero5520@gmail.com>
2023-07-26 19:00:02 +0200

    dolares

--- src/pages/panel/index.jsx
+++ src/pages/panel/index.jsx
@@ -15,7 +15,7 @@ return (
 <h1>Bienvenido a la mejor página de componentes</h1>
 
 { data?.products.map((products) => 
- <p key={products.id}>{products.name}, {products.brand}, {products.description}, {products.category}, {products.price}, <button>Añadir a cesta</button>   </p>)}
+ <p key={products.id}>{products.name}, {products.brand}, {products.description}, {products.category}, {products.price}$, <button>Añadir a cesta</button>   </p>)}
   </>
 )
  
